5cdb968f9a94150eff9fc5dbade04718a2880d5b
elzagusfi <eljagusfian@gmail.com>
2025-09-13 15:20:02 +0700

    `Refactored views and controllers for Warung Elja application, including layout, dashboard, product management, cashier, sales report, and user profile pages.`

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,8 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using WarungElja.Models;
+using WarungElja.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace WarungElja.Controllers;
 
@@ -9,14 +11,35 @@ namespace WarungElja.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly AppDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, AppDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        var totalProducts = await _context.ProductDetails.CountAsync();
+        var activeProducts = await _context.ProductDetails.CountAsync(p => p.Status == 1);
+        var totalStock = await _context.ProductStock.SumAsync(ps => (int?)ps.Stock) ?? 0;
+        var todaySales = await _context.SalesRecords
+            .Where(sr => sr.SaleDate.Date == DateTime.Today)
+            .SumAsync(sr => (int?)sr.QuantitySold) ?? 0;
+        
+        var recentSales = await _context.SalesRecords
+            .Include(sr => sr.ProductDetails)
+            .OrderByDescending(sr => sr.SaleDate)
+            .Take(5)
+            .ToListAsync();
+
+        ViewBag.TotalProducts = totalProducts;
+        ViewBag.ActiveProducts = activeProducts;
+        ViewBag.TotalStock = totalStock;
+        ViewBag.TodaySales = todaySales;
+        ViewBag.RecentSales = recentSales;
+
         return View();
     }
 
--- Controllers/SalesReportController.cs
+++ Controllers/SalesReportController.cs
@@ -22,7 +22,41 @@ namespace WarungElja.Controllers
                 .Include(s => s.ProductDetails)
                 .OrderByDescending(s => s.SaleDate)
                 .ToListAsync();
-                
+                                
+            var totalSales = salesReports.Count;
+            var totalRevenue = salesReports.Sum(s => s.Price * s.QuantitySold);
+            var todaySales = salesReports.Where(s => s.SaleDate.Date == DateTime.Today).ToList();
+            var todayRevenue = todaySales.Sum(s => s.Price * s.QuantitySold);
+            
+            var topProducts = salesReports
+                .GroupBy(s => new { s.ProductDetailsId, s.ProductDetails.ProductName })
+                .Select(g => new {
+                    ProductName = g.Key.ProductName,
+                    TotalQuantity = g.Sum(s => s.QuantitySold),
+                    TotalRevenue = g.Sum(s => s.Price * s.QuantitySold)
+                })
+                .OrderByDescending(g => g.TotalQuantity)
+                .Take(5)
+                .ToList();
+
+            var salesTrend = salesReports
+                .GroupBy(s => s.SaleDate.Date)
+                .Select(g => new {
+                    Date = g.Key.ToString("MMM dd"),
+                    TotalSales = g.Sum(s => s.QuantitySold),
+                    TotalRevenue = g.Sum(s => s.Price * s.QuantitySold)
+                })
+                .OrderBy(g => g.Date)
+                .Take(7)
+                .ToList();
+
+            ViewBag.TotalSales = totalSales;
+            ViewBag.TotalRevenue = totalRevenue;
+            ViewBag.TodaySales = todaySales.Count;
+            ViewBag.TodayRevenue = todayRevenue;
+            ViewBag.TopProducts = topProducts;
+            ViewBag.SalesTrend = salesTrend;
+
             return View(salesReports);
         }
     }
