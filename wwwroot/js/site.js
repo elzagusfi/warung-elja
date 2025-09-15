5b573fa14a4bc2dc406323334382f7b7f204e612
elzagusfi <eljagusfian@gmail.com>
2025-09-15 11:50:36 +0700

    `Refactored Sales Information section to use offcanvas component and floating button`

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Sales Information Offcanvas
+document.addEventListener('DOMContentLoaded', function () {
+    // Initialize offcanvas elements
+    var offcanvasElementList = [].slice.call(document.querySelectorAll('.offcanvas'));
+    var offcanvasList = offcanvasElementList.map(function (offcanvasEl) {
+        return new bootstrap.Offcanvas(offcanvasEl);
+    });
+});
